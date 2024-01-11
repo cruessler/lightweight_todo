affe4afcb34a443ce905c1d77ae104ac383fa389
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-01-11 13:37:59 +0100

    Fix formatting

--- test/lightweight_todo_web/controllers/error_html_test.exs
+++ test/lightweight_todo_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule LightweightTodoWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(LightweightTodoWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(LightweightTodoWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
