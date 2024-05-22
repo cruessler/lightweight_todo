9ec5d70980d3384e0a5e2e59b1eaf07774c4f4e4
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-05-22 19:03:58 +0200

    Add status to tasks

--- lib/lightweight_todo/tasks/task.ex
+++ lib/lightweight_todo/tasks/task.ex
@@ -9,13 +9,15 @@ defmodule LightweightTodo.Tasks.Task do
     field :title, :string
     field :body, :string
 
+    field :status, Ecto.Enum, values: [:created, :completed], default: :created
+
     timestamps(type: :utc_datetime)
   end
 
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :body])
+    |> cast(attrs, [:title, :body, :status])
     |> validate_required([:title, :body])
     |> assoc_constraint(:user)
   end
