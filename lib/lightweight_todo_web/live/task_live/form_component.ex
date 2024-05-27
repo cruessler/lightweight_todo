21c3ba666e8f0fda9feb9398dfc881261c316552
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-05-27 21:02:23 +0200

    Make body a <textarea>

--- lib/lightweight_todo_web/live/task_live/form_component.ex
+++ lib/lightweight_todo_web/live/task_live/form_component.ex
@@ -20,7 +20,7 @@ defmodule LightweightTodoWeb.TaskLive.FormComponent do
         phx-submit="save"
       >
         <.input field={@form[:title]} type="text" label="Title" />
-        <.input field={@form[:body]} type="text" label="Body" />
+        <.input field={@form[:body]} type="textarea" label="Body" />
         <:actions>
           <.button phx-disable-with="Saving...">Save Task</.button>
         </:actions>
