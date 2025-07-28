3f3ce664932a4884ec2b0786c59b9bfaabba0031
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-07-28 20:14:31 +0200

    Add dialog for adding sub-task

--- lib/lightweight_todo_web/live/task_live/show.ex
+++ lib/lightweight_todo_web/live/task_live/show.ex
@@ -23,12 +23,24 @@ defmodule LightweightTodoWeb.TaskLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    task = Tasks.get_task!(socket.assigns.current_user, id)
+    live_action = socket.assigns.live_action
+
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:task, Tasks.get_task!(socket.assigns.current_user, id))}
+     |> assign(:page_title, page_title(live_action))
+     |> assign(:task, task)
+     |> maybe_assign_sub_task(live_action, task)}
   end
 
   defp page_title(:show), do: "Show Task"
+  defp page_title(:new), do: "Show Task – Add Subtask"
   defp page_title(:edit), do: "Edit Task"
+
+  defp maybe_assign_sub_task(socket, :new, task) do
+    socket
+    |> assign(:sub_task, %Task{parent_id: task.id})
+  end
+
+  defp maybe_assign_sub_task(socket, _, _), do: socket
 end
