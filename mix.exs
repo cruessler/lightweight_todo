9e507daeea16ffa66f8905dcd1e6553dc62b675c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-07-31 14:27:59 +0200

    Remove floki

--- mix.exs
+++ mix.exs
@@ -42,7 +42,6 @@ defmodule LightweightTodo.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.1"},
       {:lazy_html, ">= 0.0.0", only: :test},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.2"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3.0", runtime: Mix.env() == :dev},
